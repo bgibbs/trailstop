4485e0e2a3bcf927b1edaec94b117066ae57c3c2
bob <bob@gibbs.bz>
2014-07-02 20:54:32 -0400

    Trailing stop report generator

---
+++ README.md
@@ -0,0 +1,58 @@
+Trailing Stop Monitoring Script
+--------------------------------
+
+
+Use this to cut your losers short and to let your winners run. Portfolio
+gain/loss doesn't matter only whether position is right or wrong.  This
+application will give you a summary ordered from wrong to right.
+
+The script evaluates all of your stops and sends an email update. Run it from
+crontab once per day after closing.
+
+
+------------------------------------------------------------------------
+Instructions:
+
+1. Create folio.csv.
+
+   Option 1:
+   In a text editor enter "symbol,high,low,stop" as the first line.
+   Enter your portfolio below this.
+
+   Option 2:
+   Download your portfolio from yahoo as quotes.csv or create a csv file
+   with format symbol,price.
+
+   Run the script with --start quotes.csv to generate the initial folio.csv
+
+2. Add high, low, and stop to folio.csv
+
+   a. High is the highest closing price for the security since you entered the        trade.
+
+   b. Low is the lowest closing price for the security since you entered the
+      trade.
+
+   c. Stop takes the following format:
+
+      1. Trailing for short position: NN.N%
+      2. Trailing for long position: -NN.N%
+      3. Hard stop for short position: NN.NN
+      4. Hard stop for long position: -NN.NN
+
+3. Test from the command line.
+
+   python2 trailstop.py
+
+   You should see the daily update.
+
+4. Enter it into crontab (Linux instructions)
+
+   crontab -e
+
+   0 17 * * 1-5    cd /home/<name>/Projects/trailstop; ./trailstop.py \
+                      -a <nnn>@gmail.com -p <password> 
+
+   This example runs every day at 5 PM and send the report to <nnn>@gmail.com
+
+5. If you don't use gmail, then figure out the smpt address for you mail
+   server and pass it with the --smtp option.
---
+++ trailstop.py
@@ -0,0 +1,198 @@
+#!/bin/env python2
+
+import sys
+import argparse
+import csv
+import smtplib
+from urllib2 import Request, urlopen
+
+def request(symbol, stat):
+    """from https://github.com/cgoldberg/ystockquote/blob/master/ystockquote.py
+    l1 = last trade price: closing price if after 4PM
+    p = previous close: 2 days old if after 4.
+    k = 52 wk high
+    j = 52 wk low
+    """
+    url = 'http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s' % (symbol, stat)
+    req = Request(url)
+    resp = urlopen(req)
+    content = resp.read().decode().strip()
+    return content
+
+def update(l, p):
+    stop = l['stop']
+    stl = stop.split('%')
+    stv = float(stl[0])
+    sgn = stv > 0
+    stv = abs(stv)
+    alert = report = ''
+    low = float(l['low'])
+    high = float(l['high'])
+    sym = l['symbol']
+    v = 0
+    p = max(.001, p) # prevent div by z
+    report = '%6s: %5.1f%%'
+
+    # v value is the sort criteria in percent. 
+    # Negatives are good so are listed last.
+    # Positives represent losses and deserve the most attention.
+
+    if len(stl) > 1:
+        # This is a percent trailing stop
+        if sgn:
+            # stop on price increase - short position
+            # Want to cover the short when the price increases from its low.
+            # so measure the price against the low.
+            v = (p - low)/low*100
+            if v >= stv:
+                alert = '%6s: %s stop gain triggered.' % (sym, stop)
+            if v < 0:
+                report += ' below its low.'
+            else:
+                report += ' above its low.'
+        else:
+            # Stop on price decrease - long position.
+            # Sell when price drops below its high. 
+            v = (high - p)/high*100
+            if v >= stv:
+                alert = '%6s: %s stop loss triggered.' % (sym, stop)
+
+            if v < 0:
+                report += ' above its high.'
+            else:
+                report += ' below its high.'
+
+    else:
+        # This is a hard-stop
+        if sgn:
+            # stop in price increase for a short position
+            if p >= stv:
+                alert = '%6s: has exceeded stop price of %.2f.' % (sym, stv)
+
+            v = (p - stv)/stv*100
+        else:
+            # stop on price decrease for a long position
+            if p <= stv:
+                alert = '%6s: has dropped below its stop price of %.2f.' % \
+                        (sym, stv)
+
+            v = (stv - p)/stv*100
+
+        direction = v < 0 and 'away from' or 'beyond'
+        v = abs(v)
+        report += ' %s its hard stop price of $%.2f.' % (direction, stv)
+
+    report = report % (sym, v)
+
+    l['high'] = max(high, p)
+    l['low'] = min(low, p)
+    return alert, report, v
+
+def update_all(folio):
+    alerts = ''
+    reports = []
+    d = {}
+    for l in folio:
+        p = float(request(l['symbol'], 'l1'))
+        alert, report, sortval = update(l, p)
+        if alert:
+            alerts += alert + '\n'
+        d[report] = -sortval
+        reports.append(report)
+    reports = sorted(reports, key = lambda r : d[r])
+    reports = '\n'.join(reports)
+    return alerts, reports
+
+def start(f, default_stop):
+    """Yahoo puts symbol as first field.  Price as second.
+    Rest doesn't matter.
+    """
+    folio = []
+    for l in csv.reader(f):
+        p = max(float(l[1]), .001)
+        folio.append(dict(symbol = l[0], high = p, low = p, 
+                          stop = default_stop))
+    return folio
+
+def merge(master, new):
+    s = set()
+    for l in master:
+        s.add(l['symbol'])
+    for l in new:
+        if l['symbol'] not in s:
+            master.append(l)
+
+def write(f, folio):
+    w = csv.DictWriter(f, ['symbol', 'high', 'low', 'stop'])
+    w.writeheader()
+    w.writerows(folio)
+
+def read(f):
+    folio = []
+    for l in csv.DictReader(f):
+        folio.append(l)
+    return folio
+
+def send(serv, addr, passw, subj, text):
+    server = smtplib.SMTP(serv)
+    server.starttls()
+    server.login(addr, passw)
+    server.sendmail(addr, addr, 'Subject: %s\n\n%s' % (subj, text))
+    server.quit()
+
+parser = argparse.ArgumentParser(
+        description='Initialize or update stop loss csv files and mail report.',
+        )
+parser.add_argument('-i', '--start', default = False,
+        help = 'Use this to initialize quotes from a yahoo or empty portfolio. '
+               'Anything not already in the portfolio is added to it '
+               'With a default stop and last price  as high/low');
+parser.add_argument('-f', '--folio', default = 'folio.csv',
+        help = 'Your portfolio CSV file. Initialize this file or add to it '
+               'using -yahoo option.  Edit it by hand to maintain your stop '
+               'losses')
+parser.add_argument('-s', '--default_stop', default = '-20%',
+        help = 'Default stop loss.  Used only with --start when initializing.')
+parser.add_argument('-t', '--smtp', default = 'smtp.gmail.com:587',
+        help = 'SMTP server to receive the report.')
+parser.add_argument('-a', '--addr', default = False,
+        help = 'The mail address that receives the report. If not provided '
+               'the report goes to stdout in plain text.')
+parser.add_argument('-p', '--passw', default = False,
+        help = 'SMTP send password.')
+args = parser.parse_args()
+
+try:
+    f = open(args.folio)
+    folio = read(f)
+    f.close()
+except IOError:
+    folio = []
+    if args.start == False:
+        # when initializing from a yahoo like portfolio folio.csv is not
+        # necessary
+        sys.stderr.write('Unable to open [%s] for reading.\n' % args.folio)
+        sys.exit(1)
+
+if args.start:
+    f = open(args.start)
+    new = start(f, args.default_stop)
+    merge(folio, new)
+    f.close()
+
+alerts, reports = update_all(folio)
+
+f = open(args.folio, 'w')
+write(f, folio)
+f.close()
+
+report = 'ALERTS:\n%s\n\nREPORTS:\n%s\n' % (alerts or 'NONE', reports)
+
+if args.addr and args.passw:
+    subj = '%sTrail Stop Portfolio Report' % (alerts and '[ALERTS]' or '')
+    send(args.smtp, args.addr, args.passw, subj, report)
+else:
+    print report
+
+   
+
